c29cc23ebb9fea36836f54046c402181280a5572
fedorovichmargo <fedorovich@glocalma.jp>
2024-05-24 18:12:52 +0900

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,31 @@
+import MobileNav from "@/components/ui/MobileNav";
+import SideBar from "@/components/ui/SideBar";
+import Image from "next/image";
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    const loggedIn = { firstName: 'Dude', lastName: 'JSM' }
+
+    return (
+        <main className="flex h-screen w-full font-inter">
+            <SideBar user={loggedIn} />
+
+            <div className="flex size-full flex-col">
+                <div className="root-layout">
+                    <Image
+                    src='/icons/logo.svg'
+                    width={30}
+                    height={30}
+                    alt="Logo"/>
+                    <div>
+                        <MobileNav user={loggedIn} />
+                    </div>
+                </div>
+                {children}
+            </div>
+        </main>
+    );
+}
---
+++ components/ui/RightSidebar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const RightSidebar = ({
+    user,
+    transactions,
+    banks
+}: RightSidebarProps) => {
+    return (
+        <aside className='right-sidebar'>
+            RIGHT
+        </aside>
+    )
+}
+
+export default RightSidebar
